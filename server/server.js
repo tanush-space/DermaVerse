0f7dfab148541d5cf51babdb962a6dd7f3886a2c
Mehhhhull <mehulkumarsingh.2004@gmail.com>
2026-02-27 00:23:51 +0530

    DB Connected successfully, using mongoose

--- server/server.js
+++ server/server.js
@@ -1,5 +1,10 @@
+require("dotenv").config()
 const app=require('./src/app');
+const connectDB=require('./src/config/db');
+
+//connect to database
+connectDB();
 
 app.listen(3000,()=>{
-    console.log('Server is running on port 3000');
+    console.log('Server is running on port 3000 🥳');
 })
