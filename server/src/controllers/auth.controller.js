717d90d4606c8f883e16dc7ce2f9953ff6031900
Mehhhhull <mehulkumarsingh.2004@gmail.com>
2026-03-01 23:26:05 +0530

    UTH CONTROLLER

--- server/src/controllers/auth.controller.js
+++ server/src/controllers/auth.controller.js
@@ -3,8 +3,25 @@ const jwt = require("jsonwebtoken");
 const emailService = require("../services/email.service");
 const tokenBlackListModel = require("../models/blackList.model");
 
-async function register(req, res){}
+async function register(req, res) {
+  const {name,email,password}=req.body;
 
-async function login(req, res){}
+  const isUserAlreadyExsists=userModel.findOne({
+    email
+  })
+  if(isUserAlreadyExsists){
+    return res.status(400).json({
+      message:"User Already Exsists"
+    })
+  }
+}
 
-async function logout(req, res){}
+async function login(req, res) {}
+
+async function logout(req, res) {}
+
+module.exports = {
+  register,
+  login,
+  logout,
+};
